82209db12ac3d9ea8aa3c7d9e22eecb29261d80f
Joel Hoefs <joel.hoefs7@gmail.com>
2023-06-16 00:23:40 +0800

    checking requests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import Verification from "./components/Verification.js";
 
 function App() {
-  return (
+
+  let elisa = {
+    userName: "elisa",
+    password: "elisa123",
+  }
+
+  // add someone
+  fetch("http://localhost:8080/customers", {
+      method: "post",
+      body: JSON.stringify(elisa)
+  }).then(res => res.json()).then(res => console.log(res)); 
+
+  // get all
+  fetch("http://localhost:8080/customers", {
+      method: "get"
+      // body: JSON.stringify(ob)
+  }).then(res => res.json()).then(res => console.log(res)); 
+
+
+
+  return (  
     <div className="App">
       <Verification />
     </div>
   );
-}
+} 
 
-export default App;
+export default App; 
